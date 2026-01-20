ec60d5b7a2f59bd545fee42c5106db2961fd6bdb
sidecar007-byte <sidecar007@snu.ac.kr>
2026-01-20 19:39:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ st.title("*류지성의 첫 웹앱*!")
 st.write('안녕하세요! 반갑습니다. :)')
 st.write('초록색버튼따라가기!')
 #사용자로부터 텍스트 인풋 받기
-password = st.text_input("버튼누르기")
+password = st.text_input("버튼")
+if st.button("확인")
